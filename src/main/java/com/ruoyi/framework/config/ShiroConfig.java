80242ae49c90732e29b73a262fd7deeb83f01596
fisher <gaojuntop@aliyun.com>
2019-07-26 22:24:28 +0800

    新增images过滤、thymelaf html非严格检查

--- src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -246,6 +246,7 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/docs/**", "anon");
         filterChainDefinitionMap.put("/fonts/**", "anon");
         filterChainDefinitionMap.put("/img/**", "anon");
+        filterChainDefinitionMap.put("/images/**", "anon");
         filterChainDefinitionMap.put("/ajax/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
         filterChainDefinitionMap.put("/ruoyi/**", "anon");
